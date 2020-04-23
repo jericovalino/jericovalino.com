a6a4055039fdf5403e9ca090b83af0f9c210d033
Jerico Valino <jericovalino@gmail.com>
2020-04-23 10:55:21 +0800

    innitial commit

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,11 @@
-import Nav from '../components/Nav'
+import React from 'react';
 
-const AboutPage = () => (
-  <>
-    <Nav />
-    <p>Hello, I'm the about page</p>
-  </>
-)
+const AboutPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
 
 export default AboutPage
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,31 @@
-import Nav from '../components/Nav'
+import React from 'react';
+import style from '../src/pages_styles/index.module.css';
+import UserIcon from '../src/public/vectors/userIcon';
 
-const IndexPage = () => (
-  <>
-    <Nav />
-    <p>Hello, I'm the index page</p>
-  </>
-)
+
+const WaterMark = () => {
+  return (
+    <div className={style.watermark}>
+      <text>JER</text>
+      <text>ICO</text>
+    </div>
+  )
+}
+
+const IndexPage = () => {
+  return (
+    <div className={style.Index}>
+
+      <nav className={style.sidenav}>
+        <div className={style.nav__item}>
+          <UserIcon width="40px" height="40px" />
+        </div>
+      </nav>
+
+      <WaterMark />
+
+    </div>
+  )
+}
 
 export default IndexPage
