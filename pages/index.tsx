17f7741ec27509673d32e6d03ae23474913acba4
Jerico Valino <jericovalino@gmail.com>
2020-05-14 21:25:38 +0800

    bug fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import style from '../src/pages_styles/index.module.css';
-import Head from 'next/head'
+import Head from 'next/head';
 
+import Layout from '../components/layout';
 import WaterMark from '../components/watermark';
-import Navigation from '../components/navigation';
 
 
 const IndexPage = () => {
   return (
-    <div className={style.Index}>
+    <Layout>
       <Head>
         <title>jerico</title>
       </Head>
-      <Navigation />
-      <main className={style.content}>
-        <h1>Awesome every Pixels.</h1>
-        <p>I’m a web developer from Batangas Philippines. I love building reactive and responsive website to deliver awesome experience to the user.</p>
-      </main>
-      <button className={style.mainBtn}>Send Me An Email</button>
-      <WaterMark />
-    </div>
+
+      <div className={style.Index}>
+        <main className={style.content}>
+          <h1>Awesome every Pixels.</h1>
+          <p>I’m a web developer from Batangas Philippines. I love building reactive and responsive website to deliver awesome experience to the user.</p>
+          <button className={style.mainBtn}>Send Me An Email</button>
+        </main>
+        <div className={style.waterMark}>
+          <WaterMark />
+        </div>
+      </div>
+    </Layout>
   )
 }
 
