fba4070939df02f55c5dcbd33560b8c956f19589
Jerico Valino <jericovalino@gmail.com>
2020-05-18 22:38:29 +0800

    added nametag

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,16 @@ const IndexPage = () => {
         <main className={style.content}>
           <h1>Awesome every Pixels.</h1>
           <p>I’m a web developer from Batangas Philippines. I love building reactive and responsive website to deliver awesome experience to the user.</p>
+          <div className={style.nameTag}>
+            <div className={style.dp}>
+              <img src={'/images/me.jpeg'} />
+            </div>
+            <section>
+              <h1>Jerico C. Valino</h1>
+              <p>React.js Web Developer</p>
+            </section>
+          </div>
+
           <button className={style.mainBtn}>Send Me An Email</button>
         </main>
         <div className={style.waterMark}>
