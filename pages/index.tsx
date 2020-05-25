8f07faada2158c07b86c367ac98461797e64a27a
Jerico Valino <jericovalino@gmail.com>
2020-05-25 19:08:15 +0800

    mini commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import style from '../src/pages_styles/index.module.css';
 import Head from 'next/head';
+import Link from 'next/link';
 
 import Layout from '../components/layout';
 import WaterMark from '../components/watermark';
 
+import GithubIcon from '../public/vectors/githubIcon';
+import LinkedinIcon from '../public/vectors/linkedinIcon';
 
 const IndexPage = () => {
   return (
@@ -14,6 +17,12 @@ const IndexPage = () => {
       </Head>
 
       <div className={style.Index}>
+
+        <div style={{ position: 'fixed', top: '5px', right: '5px', width: '80px', height: '30px', display: 'flex', justifyContent: 'space-between' }}>
+          <LinkedinIcon />
+          <GithubIcon />
+        </div>
+
         <main className={style.content}>
           <h1>Awesome every Pixels.</h1>
           <p>I’m a web developer from Batangas Philippines. I love building reactive and responsive website using the latest technologies available to deliver awesome experience to the user.</p>
@@ -27,7 +36,9 @@ const IndexPage = () => {
             </section>
           </div>
 
-          <button className={style.mainBtn}>Send Me An Email</button>
+          <Link href={'/portfolio'}>
+            <button className={style.mainBtn}>view portfolio</button>
+          </Link>
         </main>
         <div className={style.waterMark}>
           <WaterMark />
