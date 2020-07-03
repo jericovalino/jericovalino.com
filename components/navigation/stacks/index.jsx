3be0e50010ef60cd675917e32afda87dca9afb5b
Jerico Valino <jericovalino@gmail.com>
2020-07-03 20:56:02 +0800

    mini commit

--- components/navigation/stacks/index.jsx
+++ components/navigation/stacks/index.jsx
@@ -1,47 +1,40 @@
 import React, { useState } from 'react';
-import { stacks, main__nav__item, js, html, css, react, nodejs, psql, graphql } from './style.module.css';
+import { stacks, main__nav__item, js, html, css, react, redux } from './style.module.css';
 
 import { motion } from 'framer-motion';
-import { ReactIcon, NodeIcon, PsqlIcon, GraphqlIcon } from '../../../public/vectors/stackIcons';
+import { ReactIcon, ReduxIcon } from '../../../public/vectors/stackIcons';
 
 const Stacks = () => {
 
     const [show, setShow] = useState(false);
+    const combinedClass = (classname) => [main__nav__item, classname].join(' ');
 
     return (
         <div className={stacks}>
-            <div className={[main__nav__item, js].join(' ')}
+            <div className={combinedClass(js)}
                 onClick={() => setShow(!show)}>
                 <h1>{show ? 'JS' : 'JV'}</h1>
             </div>
-            <motion.div className={[main__nav__item, html].join(' ')}
+            <motion.div className={combinedClass(html)}
                 animate={{ left: show ? '50px' : '0px' }}>
                 <p>5</p>
                 <h1>HTML</h1>
             </motion.div>
-            <motion.div className={[main__nav__item, css].join(' ')}
+            <motion.div className={combinedClass(css)}
                 animate={{ left: show ? '100px' : '0px' }}>
                 <p>3</p>
                 <h1>CSS</h1>
             </motion.div>
-            <motion.div className={[main__nav__item, react].join(' ')}
+            <motion.div className={combinedClass(react)}
                 animate={{ left: show ? '150px' : '0px' }}>
                 <ReactIcon width="80%" height="80%" />
             </motion.div>
-            <motion.div className={[main__nav__item, nodejs].join(' ')}
+            <motion.div className={combinedClass(redux)}
                 animate={{ left: show ? '200px' : '0px' }}>
-                <NodeIcon width="80%" height="80%" />
-            </motion.div>
-            <motion.div className={[main__nav__item, psql].join(' ')}
-                animate={{ left: show ? '250px' : '0px' }}>
-                <PsqlIcon width="80%" height="80%" />
-            </motion.div>
-            <motion.div className={[main__nav__item, graphql].join(' ')}
-                animate={{ left: show ? '300px' : '0px' }}>
-                <GraphqlIcon width="80%" height="80%" />
+                <ReduxIcon width="80%" height="80%" />
             </motion.div>
         </div>
-    )
+    );
 }
 
 export default Stacks;
